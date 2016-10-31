405377fb8e1b0bad010d3cfa8cfbefa1ef35811c
zl810881283 <810881283@qq.com>
2016-11-01 00:59:46 +0800

    Update 2016-10-31T16:59:42.634Z

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,12 @@
 var gulp = require("gulp"),
   deploy = require("gulp-gh-pages");
 
+var file = require('gulp-file');
+
 gulp.task('deploy', function () {
   return gulp.src("_book/**/*.*")
-    .pipe(deploy())
+    .pipe(file('CNAME', "node.books.mafengshe.com"))
+    .pipe(deploy({cname:'node.books.mafengshe.com'}))
     .on("error", function(err){
       console.log(err)
     })
